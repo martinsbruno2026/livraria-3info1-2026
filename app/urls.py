0b6b3d2d74f7aa75b7f2a3f0c5e9e8a6f5955a82
martinsbruno2026 <martinsbru08@gmail.com>
2026-02-26 09:19:13 -0300

    update 2

--- app/urls.py
+++ app/urls.py
@@ -7,11 +7,14 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet
+from core.views import CategoriaViewSet, UserViewSet
 
 router = DefaultRouter()
 
+router.register(r'categoria', CategoriaViewSet, basename='categoria')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
